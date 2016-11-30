a527e4c2c02dbc276d97f1a30abeef1078f2fdaa
Steve T <stevethewebguy@gmail.com>
2016-11-29 21:55:48 -0800

    continued dev

--- app/code/local/Atwix/Cmsattr/Block/List.php
+++ app/code/local/Atwix/Cmsattr/Block/List.php
@@ -7,14 +7,17 @@ class Atwix_Cmsattr_Block_List extends Mage_Catalog_Block_Product_Abstract
     {
         $_product = $this->getProduct();
         $productType = $_product->getProduct_type();
+        $podsProductType = $_product->getPods_product_type();
         $product_id = $_product->getId();
 
         if (is_null($this->_itemCollection)) {
-            $this->_itemCollection = Mage::getModel('atwix_cmsattr/products')->getItemsCollection($productType);
+            $this->_itemCollection = Mage::getModel('atwix_cmsattr/products')->getItemsCollection($productType,$podsProductType,$product_id);
         }
 
-        echo '<br>current product id: ' . $product_id;
-        echo '<br>current product_type: ' . $productType;
+//        echo '<br>current product id: ' . $product_id;
+//        echo '<br>current product_type: ' . $productType;
+//        echo '<br>current pods_product_type: ' . $podsProductType;
+
         return $this->_itemCollection;
     }
 }
--- app/code/local/Atwix/Cmsattr/Model/Products.php
+++ app/code/local/Atwix/Cmsattr/Model/Products.php
@@ -1,15 +1,33 @@
 <?php
 class Atwix_Cmsattr_Model_Products extends Mage_Catalog_Model_Product
 {
-    public function getItemsCollection($valueId)
+    public function getItemsCollection($valueId,$podsProductType,$product_id)
     {
-        $collection = $this->getCollection()
-            ->addAttributeToSelect('*')
-            ->addAttributeToFilter('product_type', array('eq' => $valueId));
+
         //uses $valueId to load dif related product_type
+        $collection = $this->getCollection();
+
+        if(!$valueId){
+            $collection
+                ->addAttributeToSelect('*')
+                ->addAttributeToFilter('pods_product_type', array('eq' => $podsProductType))
+                ->getSelect()->limit(5);
+        }
+        else{
+            $collection
+                ->addAttributeToSelect('*')
+                ->addAttributeToFilter('product_type', array('eq' => $valueId))
+                ->getSelect()->limit(5);
+        }
+        //filter featured product from being displayed in its own cross sells
+        $collection
+            ->addAttributeToSelect('entity_id')
+            ->addAttributeToFilter('entity_id', array('neq' => $product_id));
+
         Mage::getSingleton('cataloginventory/stock')->addInStockFilterToCollection($collection);
 
-        echo 'current product_type: ' . $valueId;
+       echo 'current id: ' . $product_id;
+
         return $collection;
     }
 }
